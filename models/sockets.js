a6c69b61b3ba916590a9bea71bf89271dd9834a4
Johan David <johancarras16@gmail.com>
2021-01-15 15:49:34 -0500

    aplicacion terminada

---
+++ models/sockets.js
@@ -0,0 +1,39 @@
+const Marcadores = require("./marcadores");
+
+
+class Sockets {
+
+    constructor(io) {
+
+        this.io = io;
+
+        this.marcadores = new Marcadores();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', (socket) => {
+            
+            // marcadores-activos
+            socket.emit('marcadores-activos', this.marcadores.activos);
+
+            // marcador-nuevo
+            socket.on('marcador-nuevo', (marcador) => {
+                this.marcadores.agregarMarcador(marcador);
+                socket.broadcast.emit('marcador-nuevo', marcador);
+            });          
+
+            socket.on('marcador-actualizado', (marcador) => {
+                this.marcadores.actualizarMarcador( marcador );
+                socket.broadcast.emit('marcador-actualizado', marcador);
+            });
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
